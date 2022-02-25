48a2b0e0ec85996a9200c972389ac8e190da1544
Garrett Molle <46017064+gmolle@users.noreply.github.com>
2022-02-25 15:50:01 -0500

    Added header to products page

--- client-side/src/components/ProductsPage/ProductsPage.jsx
+++ client-side/src/components/ProductsPage/ProductsPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 function ProductsPage() {
-  return <div>Products</div>;
+  return <Typography variant="h2">Products</Typography>;
 }
 
 export default ProductsPage;
