fe0f9eeb38a5ff488cc60755120402d9dd92d1bc
Garrett Molle <46017064+gmolle@users.noreply.github.com>
2022-02-28 16:29:53 -0500

    WIP started working on available products story

--- client-side/src/components/ProductsPage/ProductsPage.jsx
+++ client-side/src/components/ProductsPage/ProductsPage.jsx
@@ -5,15 +5,17 @@ import React, { useState } from "react";
 import { Typography } from "@mui/material";
 
 function ProductsPage() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([1]);
 
   return (
     <div id="products-page">
       <Typography variant="h2">Products</Typography>
-      {products.length === 0 && (
+      {products.length === 0 ? (
         <Typography variant="h3">
           No Products Available. Please check back again!
         </Typography>
+      ) : (
+        <Typography variant="h3">Available Products</Typography>
       )}
     </div>
   );
