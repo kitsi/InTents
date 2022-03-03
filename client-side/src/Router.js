969bb04651a7a7d3cc55a7151bf5318ce9670b50
Pallavi <pallavidesh20@gmail.com>
2022-03-03 14:58:13 -0600

    Add link to cartpage from productpage

--- client-side/src/Router.js
+++ client-side/src/Router.js
@@ -1,10 +1,13 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
+
+import CartPage from "./components/CartPage/CartPage";
 import ProductsPage from "./components/ProductsPage/ProductsPage";
+import React from "react";
 
 function Router() {
   return (
     <Routes>
+      <Route path="/cart" element={<CartPage />} />
       <Route path="/products" element={<ProductsPage />} />
     </Routes>
   );
