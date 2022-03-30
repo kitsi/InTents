df28ab6cb4f1b38fb6e5fe64aa54b2e1d194f920
KS <kitsidhu@gmail.com>
2022-03-30 15:53:42 -0400

    clean up unnecessary yup validations

--- client-side/src/components/PaymentPage/PaymentFormSchema.jsx
+++ client-side/src/components/PaymentPage/PaymentFormSchema.jsx
@@ -1,15 +1,10 @@
-import React from "react";
 import * as yup from "yup";
 
 function PaymentFormSchema() {
   const schema = yup.object().shape({
     firstName: yup.string().required("First name is required"),
     lastName: yup.string().required("Last name is required"),
-    cardNumber: yup
-      .string()
-      .max(16)
-      .matches()
-      .required("Card number is required"),
+    cardNumber: yup.string().max(16).required("Card number is required"),
     expirationCheck: yup.string().required("Expiration is required"),
     cvcCheck: yup.string().required("CVC is required"),
   });
