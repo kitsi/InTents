f6c37c6511bf0351be04e0147179b168f2dd787c
Matthew Nordstrand <matt_nordstrand@live.com>
2022-04-08 15:14:12 -0400

    Footer placed on bottom of products and cart pages

--- client-side/src/components/Footer/Footer.jsx
+++ client-side/src/components/Footer/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
+
+import * as styles from "./FooterStyles";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTwitter, faFacebook, faInstagram, faYoutube} from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <Box sx={styles.container}>
+      <Typography>
+      &copy; 2022
+      </Typography>
+    </Box>
   )
 }
---
+++ client-side/src/components/Footer/FooterStyles.js
@@ -0,0 +1,6 @@
+export const container = {
+  width: "100%",
+  backgroundColor: "secondary.main",
+  color: "white",
+  padding: "1rem",
+};
