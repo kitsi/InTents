d0e6de510b60d755d7616453aeb02a8bb3a09689
Matthew Nordstrand <matt_nordstrand@live.com>
2022-04-08 15:23:40 -0400

    Added socials to footer

--- client-side/src/components/Footer/Footer.jsx
+++ client-side/src/components/Footer/Footer.jsx
@@ -3,12 +3,21 @@ import { Box, Typography } from "@mui/material";
 
 import * as styles from "./FooterStyles";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faTwitter, faFacebook, faInstagram, faYoutube} from "@fortawesome/free-solid-svg-icons";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Footer() {
   return (
     <Box sx={styles.container}>
+      <Box sx={styles.socialContainer}>
+        <Typography>Follow Us</Typography>
+        <YouTubeIcon />
+        <FacebookIcon />
+        <TwitterIcon />
+        <InstagramIcon />
+      </Box>
       <Typography>
       &copy; 2022
       </Typography>
--- client-side/src/components/Footer/FooterStyles.js
+++ client-side/src/components/Footer/FooterStyles.js
@@ -1,6 +1,13 @@
 export const container = {
+  display: "flex",
+  justifyContent: "space-between",
   width: "100%",
   backgroundColor: "secondary.main",
   color: "white",
   padding: "1rem",
+};
+
+export const socialContainer = {
+  display: "flex",
+  gap: "0.5rem",
 };
