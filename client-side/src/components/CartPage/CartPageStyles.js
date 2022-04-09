dbd4f442384851ad847eed56c387a89dfff28775
donavynhaley <donavynhaley@gmail.com>
2022-04-09 14:06:34 -0700

    WIP cart page

---
+++ client-side/src/components/CartPage/CartPageStyles.js
@@ -0,0 +1,44 @@
+export const productTilesContainer = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "3rem",
+  marginTop: "2rem",
+};
+
+export const buttonContainer = {
+  margin: "0rem auto",
+  top: "56px",
+  zIndex: "10",
+  backgroundColor: "#fff",
+  padding: "1rem 0rem",
+  position: "sticky",
+  boxShadow:
+    "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 2px 3px 0px rgb(0 0 0 / 12%)",
+};
+
+export const buttonContainerMobile = {
+  top: "64px",
+};
+
+export const buttons = {
+  maxWidth: "1000px",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  margin: "0 auto",
+  padding: "0 2rem",
+};
+
+export const subtotal = {
+  textAlign: "right",
+  margin: "0",
+  padding: "0 2rem",
+  maxWidth: "1000px",
+  margin: "1rem auto 0 auto",
+};
+
+export const contentContainer = {
+  padding: "0 2rem",
+  maxWidth: "1000px",
+  margin: "0 auto",
+};
