cc1694a1b0862fcf60a9154b2a5d501b0affcf36
donavynhaley <donavynhaley@gmail.com>
2022-04-09 12:38:23 -0700

    Converted admin page

--- client-side/src/components/AdminPage/AdminPageStyles.js
+++ client-side/src/components/AdminPage/AdminPageStyles.js
@@ -1,6 +1,21 @@
 export const addButton = {
-  maxWidth: "1000px", 
-  margin: "auto", 
+  maxWidth: "1000px",
+  margin: "auto",
   padding: "1rem 2rem 0 2rem",
-  textAlign: "right" 
-}
+  textAlign: "right",
+};
+
+export const pageHeader = {
+  padding: "1rem 0",
+};
+
+export const productTilesContainer = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "3rem",
+  maxWidth: "1000px",
+  margin: "0 auto",
+  padding: "0 2rem",
+  marginTop: "2rem",
+};
