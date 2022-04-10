c84e4ec431b646e28ca0e1c79e3323fabe07d57a
donavynhaley <donavynhaley@gmail.com>
2022-04-10 14:30:09 -0700

    Implemented redux persists on the cart reducer. Cart items are now persisted in local storage. Tested on multiple browsers

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- client-side/src/redux/store.js
+++ client-side/src/redux/store.js
@@ -1,12 +1,39 @@
 import cartReducer from "../components/CartPage/cartSlice";
 import checkoutReducer from "../components/CheckoutPage/checkoutSlice";
-import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../components/ProductsPage/productsSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
-    cart: cartReducer,
+    cart: persistedCartReducer,
     checkout: checkoutReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
