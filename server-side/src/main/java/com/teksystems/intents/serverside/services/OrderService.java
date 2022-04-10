c07484cfee8bf322b0078874016e805b52b5dc93
KS <kitsidhu@gmail.com>
2022-04-09 22:10:25 -0400

    add OrderService with getOrders method

---
+++ server-side/src/main/java/com/teksystems/intents/serverside/services/OrderService.java
@@ -0,0 +1,21 @@
+package com.teksystems.intents.serverside.services;
+
+import com.teksystems.intents.serverside.models.Inventory;
+import com.teksystems.intents.serverside.models.Order;
+import com.teksystems.intents.serverside.repositories.OrderRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepo;
+
+    public Page<Order> getOrders(int pageNum, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNum, pageSize);
+        Page<Order> orders = orderRepo.findAll(pageable);
+        return orders;
+    }
+}
