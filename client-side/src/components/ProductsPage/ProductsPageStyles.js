f3b7353493dc4990d5bff6ebff38483161a62a7f
donavynhaley <donavynhaley@gmail.com>
2022-04-10 11:20:33 -0700

    Converted ProductsPage

---
+++ client-side/src/components/ProductsPage/ProductsPageStyles.js
@@ -0,0 +1,18 @@
+export const alignCenter = {
+  textAlign: "center",
+};
+
+export const productsPageHeader = {
+  padding: "1rem 0",
+};
+
+export const productTilesContainer = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "3rem",
+  padding: "0 2rem 2rem 0",
+  maxWidth: "1000px",
+  margin: "0 auto",
+  marginTop: "2rem",
+};
