c36473365448cec9ac0fb3f71616f5ad50ebed50
Neo Maraisane <maraisanen@gmail.com>
2022-04-10 22:56:34 -0400

    Implement functionality to confirm a payment and charge a customer

--- client-side/src/components/CheckoutPage/Forms/PaymentForm.jsx
+++ client-side/src/components/CheckoutPage/Forms/PaymentForm.jsx
@@ -71,10 +71,47 @@ const PaymentForm = (props, { handleModalOpen }) => {
     });
   };
 
-  console.log(getClientSecret({ amount: 10 }));
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault(e);
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const clientSecret = await getClientSecret({
+      amount: 10,
+    });
+
+    console.log(clientSecret);
+
+    const cardElement = elements.getElement(CardNumberElement);
+
+    const { error } = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: cardElement,
+      },
+    });
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occured.");
+    }
+
+    setIsLoading(false);
   };
 
   return (
@@ -168,10 +205,13 @@ const PaymentForm = (props, { handleModalOpen }) => {
             sx={{
               marginTop: "1rem",
             }}
+            disabled={isLoading || !stripe || !elements}
           >
             Place Order
           </Button>
         </Box>
+        {/* Show any error or success messages  */}
+        {message && <div id="payment-message">{message}</div>}
       </form>
     </Container>
   );
