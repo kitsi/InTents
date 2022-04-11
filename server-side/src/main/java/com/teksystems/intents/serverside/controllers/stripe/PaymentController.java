3f04b82a149985238077c06bf0a88c2fc656d047
Neo Maraisane <maraisanen@gmail.com>
2022-04-10 21:38:53 -0400

    Add post method and cross origin annotation for CORS support

--- server-side/src/main/java/com/teksystems/intents/serverside/controllers/stripe/PaymentController.java
+++ server-side/src/main/java/com/teksystems/intents/serverside/controllers/stripe/PaymentController.java
@@ -5,15 +5,14 @@ import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.teksystems.intents.serverside.dto.CreatePayment;
 import com.teksystems.intents.serverside.dto.CreatePaymentResponse;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http:localhost:3000")
 @RestController
 @RequestMapping("/api/create-payment-intent")
 public class PaymentController {
+    @PostMapping("/")
+    @CrossOrigin(origins = "http://localhost:3000")
     public CreatePaymentResponse createPaymentIntent(@RequestBody CreatePayment createPayment) throws StripeException {
         PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
                 .setCurrency("usd")
