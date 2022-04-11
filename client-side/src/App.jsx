5dd519e8c3a81a9ce5a56e39388b69a750521fd2
Neo Maraisane <maraisanen@gmail.com>
2022-04-10 20:35:20 -0400

    refactor: move Stripe Elements provider to be accesible only to the checkout page

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -4,20 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import theme from "./theme";
 import PageContainer from "./PageContainer";
 
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-
-const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
-
 function App() {
   return (
     <div className="App">
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <PageContainer />
-          </Elements>
+          <PageContainer />
         </BrowserRouter>
       </ThemeProvider>
     </div>
--- client-side/src/Router.js
+++ client-side/src/Router.js
@@ -7,16 +7,27 @@ import React from "react";
 import ProductDetailsPage from "./components/ProductDetailsPage/ProductDetailsPage";
 import LandingPage from "./components/LandingPage/LandingPage";
 import AdminPage from "./components/AdminPage/AdminPage";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />}/>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/products/categories/:category" element={<ProductsPage />} />
       <Route path="/admin" element={<AdminPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route
+        path="/checkout"
+        element={
+          <Elements stripe={stripePromise}>
+            <CheckoutPage />
+          </Elements>
+        }
+      />
       <Route path="/product/:sku" exact element={<ProductDetailsPage />} />
     </Routes>
   );
