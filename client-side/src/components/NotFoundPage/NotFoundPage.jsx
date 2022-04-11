9f812bb89f510b11daaeefcb399037d58cd0c407
donavynhaley <donavynhaley@gmail.com>
2022-04-10 21:52:09 -0700

    Finished 404 page styling with back button.

--- client-side/src/components/NotFoundPage/NotFoundPage.jsx
+++ client-side/src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { Box, Container, Typography, Button, Divider } from "@mui/material";
+import { Link } from "react-router-dom";
+import * as styles from "./NotFoundPageStyles";
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <Container sx={styles.pageContainer}>
+      <Box sx={styles.header}>
+        <Typography variant="h2">404 Something Went Wrong</Typography>
+        <Divider />
+      </Box>
+      <img
+        width={"95%"}
+        src="https://i.imgur.com/hBQsejY.png"
+        alt="Page Not Found"
+      ></img>
+      <Button sx={styles.button} variant="contained" component={Link} to="/">
+        Go To Home Page
+      </Button>
+    </Container>
+  );
 }
 
 export default NotFoundPage;
---
+++ client-side/src/components/NotFoundPage/NotFoundPageStyles.js
@@ -0,0 +1,14 @@
+export const pageContainer = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "2rem",
+};
+
+export const button = {
+  marginTop: "2rem",
+};
+
+export const header = {
+  marginBottom: "2rem",
+};
