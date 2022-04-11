0c85419162621a33c93594993e3f4ae0cffbe62f
Matthew Nordstrand <matt_nordstrand@live.com>
2022-04-11 06:47:48 -0400

    Added currency formatting to admin page.

--- client-side/src/components/AdminPage/AdminProduct.jsx
+++ client-side/src/components/AdminPage/AdminProduct.jsx
@@ -12,6 +12,7 @@ import { useDispatch } from "react-redux";
 import { fetchProducts } from "../ProductsPage/productsSlice";
 import * as styles from "../common/ProductTileStyles";
 import { baseUrl } from "../../utilities/strings";
+import formatCurrency from "../../utilities/formatCurrency";
 
 function AdminProduct({ productData, editProduct }) {
   const dispatch = useDispatch();
@@ -35,7 +36,7 @@ function AdminProduct({ productData, editProduct }) {
           {productData.name}
         </Typography>
         <Typography variant="body1" color="text.secondary">
-          ${productData.price}
+          {formatCurrency(productData.price)}
         </Typography>
         <Typography
           variant="body2"
