398d799cb52077afdbcb32d224983df9ba04bbdb
Matthew Nordstrand <matt_nordstrand@live.com>
2022-04-11 12:55:55 -0400

    Fixed cart rendering contents when changing

--- client-side/src/components/CartPage/CartPage.jsx
+++ client-side/src/components/CartPage/CartPage.jsx
@@ -1,29 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as styles from "./CartPageStyles.js";
-
+import axios from "axios";
 import { useSelector } from "react-redux";
 import { Button, Box, Typography } from "@mui/material";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { Link } from "react-router-dom";
 import formatCurrency from "../../utilities/formatCurrency.js";
 import CartProductTile from "./CartProductTile";
+import { baseUrl } from "../../utilities/strings.js";
+import Loading from "../common/Loading";
 
 export default function CartPage() {
   const { cartItems } = useSelector((state) => state.cart);
+  const [products, setProducts] = useState([]);
+  const [cartList, setCartList] = useState();
 
-  const cartList = cartItems.map((item) => {
-    return <CartProductTile key={item.id} product={item} />;
-  });
+  const updateCartList = () => {
+    setCartList(cartItems.map((item) => {
+      const product = products.filter(product => product.id === item.id)[0];
+      return <CartProductTile key={item.id} cartProduct={item} product={product} />;
+    })
+  )
+};
+
+  useEffect(() => {
+    updateAllProducts();
+  }, [cartItems]);
+
+  useEffect(() => {
+    updateCartList();
+  }, [products]);
+
+  const updateAllProducts = async () => {
+    const productArray = [];
+
+    for (let i = 0; i < cartItems.length; i++) {
+      const { data } = await axios.get(`${baseUrl}/products/${cartItems[i].id}`);
+      productArray.push(data);
+    }
+
+    setProducts(productArray);
+  }
 
   function getSubtotal() {
     let total = 0;
     for (let i = 0; i < cartItems.length; i++) {
-      total += cartItems[i].product.price * cartItems[i].quantity;
+      const cartItem = cartItems[i];
+      const product = products.filter(item => item.id === cartItem.id)[0];
+
+      if (product) {
+        total += product.price * cartItem.quantity;
+      }
     }
     return total;
   }
 
   return (
+    products.length === cartItems.length ?
     <Box>
       <Box sx={styles.buttonContainer}>
         <Box sx={styles.buttons}>
@@ -63,5 +96,6 @@ export default function CartPage() {
         </Box>
       </Box>
     </Box>
+    : <Loading />
   );
 }
