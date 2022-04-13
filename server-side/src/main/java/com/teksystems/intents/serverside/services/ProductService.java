ee7eec53af7bebe8af6e207dc025c7799797b7a4
donavynhaley <donavynhaley@gmail.com>
2022-04-13 12:02:17 -0700

    Merged with develop removed unessesary import

--- server-side/src/main/java/com/teksystems/intents/serverside/services/ProductService.java
+++ server-side/src/main/java/com/teksystems/intents/serverside/services/ProductService.java
@@ -3,6 +3,7 @@ package com.teksystems.intents.serverside.services;
 import com.teksystems.intents.serverside.models.Category;
 import com.teksystems.intents.serverside.models.Inventory;
 import com.teksystems.intents.serverside.models.Product;
+import com.teksystems.intents.serverside.repositories.CategoryRepository;
 import com.teksystems.intents.serverside.repositories.InventoryRepository;
 import com.teksystems.intents.serverside.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
