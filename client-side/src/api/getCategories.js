7022a065fe5fb134eb3478bafefc6df6956d6cbd
Matthew Nordstrand <matt_nordstrand@live.com>
2022-04-12 21:05:47 -0400

    Added temporary workaround for categories until endpoint is supported

--- client-side/src/api/getCategories.js
+++ client-side/src/api/getCategories.js
@@ -2,10 +2,33 @@ import axios from "axios";
 import { baseUrl } from "../utilities/strings";
 
 export default async function getCategories() {
-  await axios.get(`${baseUrl}/categories`)
-    .then(res => data = res.data);
+  // await axios.get(`${baseUrl}/categories`)
+  //   .then(res => data = res.data);
 
-  const categories = data;
+  // const categories = data;
+
+  const categories = [
+    {
+      id: 1,
+      title: "cookware"
+    },
+    {
+      id: 2,
+      title: "tents"
+    },
+    {
+      id: 3,
+      title: "sleeping-bags"
+    },
+    {
+      id: 4,
+      title: "fans"
+    },
+    {
+      id: 5,
+      title: "emergency"
+    },
+  ];
 
   return categories;
 }
