61d7d088ed7056c334797f03740d33cea9f19a89
Matthew Nordstrand <matt_nordstrand@live.com>
2022-04-14 11:57:22 -0400

    Added helper function to get orders from backend

---
+++ client-side/src/api/getOrders.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { baseUrl } from "../utilities/strings";
+
+export default async function getOrders(pageNumber = 0, limit = 10) {
+  let data;
+  let success = true;
+
+  await axios
+    .get(`${baseUrl}/orders/?pageNum=${pageNumber}&pageSize=${limit}`)
+    .then((res) => (data = res.data))
+    .catch(() => (success = false));
+
+  const orders = data?.content;
+  const totalPages = data?.totalPages;
+
+  return { orders, success, totalPages, pageNumber };
+}
