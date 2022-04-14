35623f9640abd663f478e23e26cc4219c641e8cb
Garrett Molle <46017064+gmolle@users.noreply.github.com>
2022-04-14 18:33:26 -0400

    Fixed key error and added optional chaining to allow the page to render without the billing being setup on the backend

--- client-side/src/components/OrderHistoryPage/SingleOrder.jsx
+++ client-side/src/components/OrderHistoryPage/SingleOrder.jsx
@@ -5,23 +5,49 @@ import * as styles from "./SingleOrderStyles";
 import formatCurrency from "../../utilities/formatCurrency";
 
 export default function SingleOrder({ order }) {
-
-  const productList = order.orderProducts.map(orderProduct => {
-    return <OrderProduct key={orderProduct.OrderProductId} orderProduct={orderProduct} />;
+  const productList = order.orderProducts.map((orderProduct) => {
+    return (
+      <OrderProduct
+        key={orderProduct.orderProductId}
+        orderProduct={orderProduct}
+      />
+    );
   });
+
+  console.log(order);
 
   return (
     <Paper sx={styles.paper}>
       <Box sx={styles.userContainer}>
-        <Typography><span style={styles.label}>Order # </span>{order.orderId}</Typography>
-        <Typography><span style={styles.label}>User: </span>{order.user.username} ({order.user.firstName} {order.user.lastName})</Typography>
-        <Typography><span style={styles.label}>Paid: </span>{order.billing.paid ? "Yes" : "No"}</Typography>
-        <Typography><span style={styles.label}>Subtotal: </span>{formatCurrency(order.billing.subtotal)}</Typography>
-        <Typography><span style={styles.label}>Tax: </span>{formatCurrency(order.billing.tax)}</Typography>
-        <Typography><span style={styles.label}>Total: </span>{formatCurrency(order.billing.subtotal + order.billing.tax)}</Typography>
+        <Typography>
+          <span style={styles.label}>Order # </span>
+          {order.orderId}
+        </Typography>
+        <Typography>
+          <span style={styles.label}>User: </span>
+          {order.user.username} ({order.user.firstName} {order.user.lastName})
+        </Typography>
+        <Typography>
+          <span style={styles.label}>Paid: </span>
+          {order?.billing?.paid ? "Yes" : "No"}
+        </Typography>
+        <Typography>
+          <span style={styles.label}>Subtotal: </span>
+          {formatCurrency(order?.billing?.subtotal)}
+        </Typography>
+        <Typography>
+          <span style={styles.label}>Tax: </span>
+          {formatCurrency(order?.billing?.tax)}
+        </Typography>
+        <Typography>
+          <span style={styles.label}>Total: </span>
+          {formatCurrency(order?.billing?.subtotal + order?.billing?.tax)}
+        </Typography>
       </Box>
       <Box sx={styles.productsContainer}>
-        <Typography sx={{...styles.label, ...styles.centerText}}>Products</Typography>
+        <Typography sx={{ ...styles.label, ...styles.centerText }}>
+          Products
+        </Typography>
         {productList}
       </Box>
     </Paper>
