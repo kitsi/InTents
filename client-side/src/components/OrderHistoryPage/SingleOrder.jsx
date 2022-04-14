855cfa0c70ae0917e083df49aa270b8f236ef2a1
Matthew Nordstrand <matt_nordstrand@live.com>
2022-04-14 12:57:10 -0400

    Added basic info about users in their order details panel

--- client-side/src/components/OrderHistoryPage/SingleOrder.jsx
+++ client-side/src/components/OrderHistoryPage/SingleOrder.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, Paper, Typography } from "@mui/material";
+import * as styles from "./SingleOrderStyles";
 
 export default function SingleOrder({ order }) {
   return (
-    <div>SingleOrder</div>
+    <Paper sx={styles.paper}>
+      <Box sx={styles.userContainer}>
+        <Typography>Order #{order.orderId}</Typography>
+        <Typography>User: {order.user.username} ({order.user.firstName} {order.user.lastName})</Typography>
+      </Box>
+      <Box sx={styles.productsContainer}>
+        Products here
+      </Box>
+    </Paper>
   );
 }
---
+++ client-side/src/components/OrderHistoryPage/SingleOrderStyles.js
@@ -0,0 +1,21 @@
+export const paper = {
+  margin: "1rem",
+  padding: "0.5rem",
+  display: "flex",
+  flexDirection: {
+    xs: "column",
+    lg: "row",
+  },
+};
+
+export const userContainer = {
+  width: {
+    xs: "100%",
+    lg: "50%",
+  },
+};
+
+export const productsContainer = {
+  width: "100%",
+  border: "1px solid black",
+};
